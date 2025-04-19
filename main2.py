a7cc10df70c8472dfcc1586e02f86a90370d86b8
Fatim-coding <timabadjie12@gmail.com>
2025-04-19 15:32:03 +0000

    second commit

---
+++ main2.py
@@ -0,0 +1,15 @@
+first_name = "codingal"
+last_name =  "Educations"
+full_name = first_name+last_name
+example = "Haa"*5
+
+print("First Name :", first_name)
+print("Last Name  :", last_name)
+print("Full Name :", full_name)
+print("string Multiplied 5 times gives this result :", example)
+
+word = 'coding'
+print("Length of string :", len(word))
+print("First Letter of string :", word[0])
+print("Last Letter of string :", word[5])
+print("string sliced :", word[0:3])
