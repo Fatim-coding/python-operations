0d0f320cb2af3f9b8b1175f3b3517283cdafe0be
Fatim-coding <timabadjie12@gmail.com>
2025-04-19 15:38:20 +0000

    third commit

---
+++ main3.py
@@ -0,0 +1,11 @@
+# Take input values from user
+x = input("Enter value of x:")
+y = input("Enter value of y:")
+
+# swapping
+teep = x
+x = y
+y = teep
+
+print("value of x after swapping", x)
+print("value of y after swapping", y)
